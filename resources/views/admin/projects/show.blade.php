a83a743fa05831226c31d23f1f008407dc21b364
giuliagrfo <giuliagrifo1@gmail.com>
2023-01-17 17:54:45 +0100

    implementation projects.show

--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -5,9 +5,20 @@
 <hr>
 <h5>Slug: {{$project->slug}}</h5>
 <img src="{{asset('storage/' . $project->cover_image)}}" alt="">
-<div class="type">
+<div class="types">
     <strong>Type:</strong>
     {{ $project->type ? $project->type->name : 'Uncategorized'}}
+</div>
+<div class="technologies">
+    <strong>Technologies:</strong>
+    @if(count($project->technologies) > 0)
+    @foreach($project->technologies as $technology)
+    <span>{{$technology->name}}</span>
+    @endforeach
+
+    @else
+    <span>Not technologies associated to the current project</span>
+    @endif
 </div>
 <p>Description: {{$project->description}}</p>
 @endsection
