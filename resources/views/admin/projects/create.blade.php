153b8fa3eedf558c46dd2b670490e46393db675b
giuliagrfo <giuliagrifo1@gmail.com>
2023-01-17 17:41:16 +0100

    Add technologies in create

--- resources/views/admin/projects/create.blade.php
+++ resources/views/admin/projects/create.blade.php
@@ -19,6 +19,9 @@
         <input type="file" name="cover_image" id="cover_image" class="form-control @error('cover_image') is-invalid @enderror" placeholder="" aria-describedby="cover_imageHelper">
         <small id="cover_imageHelper" class="text-muted">Add an image</small>
     </div>
+    @error('cover_image')
+    <div class="alert alert-danger">{{$message}}</div>
+    @enderror
     <div class="mb-3">
         <label for="type_id" class="form-label">Types</label>
         <select class="form-select form-select-lg @error('type_id') 'is-invalid' @enderror" name="type_id" id="type_id">
@@ -30,12 +33,35 @@
 
         </select>
     </div>
+    @error('types')
+    <div class="alert alert-danger">{{$message}}</div>
+    @enderror
+    <div class="mb-3">
+        <label for="technologies" class="form-label">Technologies</label>
+        <select multiple class="form-select form-select-lg" name="technologies[]" id="technologies">
+            <option value="" disabled>Select a Technology</option>
 
+            @foreach ($technologies as $technology )
+            <option value="{{$technology->id}}" {{ in_array('technology_id'), old('technologies', [])) ? 'selected' : '' }}>{{$technology->name}}</option>
+            @else
+            <option value="{{$technology->id}}">{{$technology->name}}</option>
+            @endif
+            @empty
+            <option value="" disabled>No Technologies available</option>
+            @endforeach
+        </select>
+        @error('technologies')
+        <div class="invalid-feedback">{{ $message }}</div>
+        @enderror
+    </div>
     <div class="mb-3">
         <label for="description" class="form-label">Description</label>
         <textarea type="text" name="description" id="description" class="form-control" placeholder="" aria-describedby="descriptionHelper"></textarea>
         <small id="descriptionHelper" class="text-muted">Add a description with max 300 characters</small>
     </div>
+    @error('description')
+    <div class="alert alert-danger">{{$message}}</div>
+    @enderror
     <button type="submit" class="btn btn-primary">Add</button>
 </form>
 @endsection
